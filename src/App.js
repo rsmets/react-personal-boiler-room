bda14660a7f05bce89206867dbc94b9790f8b4b0
Ray Smets <rayjsmets@gmail.com>
2018-05-23 19:21:37 -0700

    [sidebar] dummy added.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
+import Sidebar from 'react-sidebar';
+import SidebarContent from './SidebarContent.js'
 import logo from './logo.svg';
 import './App.css';
 import Listing from './Listing.js';
 import './styling.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sidebarOpen: true
+    }
+
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
   componentDidMount() {
     //debugger;
+  }
+
+  onSetSidebarOpen(open) {
+    this.setState({sidebarOpen: open});
   }
 
   render() {
     return (
       <div className="App">
-        <Listing/>
+        {/* <Listing/> */}
+        <Sidebar 
+          // sidebar={<Listing/>}
+          sidebar={<SidebarContent/>}
+          open={this.state.sidebarOpen}
+          onSetOpen={this.onSetSidebarOpen}>
+          <b>Main content</b>
+        </Sidebar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to a generic landing page</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          More coming soon... hopefully.
         </p>
       </div>
     );
